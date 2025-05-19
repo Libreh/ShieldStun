4c9410012ef8e8b937317051d126213df669783c
Libreh <beverlydlashley@protonmail.com>
2025-05-19 15:59:39 +0200

    Rename MOD_ID to ID

--- src/main/java/me/libreh/shieldstun/ModInit.java
+++ src/main/java/me/libreh/shieldstun/ModInit.java
@@ -8,8 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ModInit implements ModInitializer {
-	public static final String MOD_ID = "shieldstun";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final String ID = "shieldstun";
+	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
 
     @Override
 	public void onInitialize() {
--- src/main/java/me/libreh/shieldstun/util/Utils.java
+++ src/main/java/me/libreh/shieldstun/util/Utils.java
@@ -4,6 +4,6 @@ import me.libreh.shieldstun.ModInit;
 
 public class Utils {
     public static String formatPermission(String key) {
-        return "%s.%s".formatted(ModInit.MOD_ID, key);
+        return "%s.%s".formatted(ModInit.ID, key);
     }
 }
