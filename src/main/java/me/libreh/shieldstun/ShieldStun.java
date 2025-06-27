792934fbe40fe475c5b80c62fda3eac7c53b06d7
Libreh <beverlydlashley@protonmail.com>
2025-06-28 00:02:19 +0200

    Refactor code

--- src/main/java/me/libreh/shieldstun/ShieldStun.java
+++ src/main/java/me/libreh/shieldstun/ShieldStun.java
@@ -7,14 +7,13 @@ import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ModInit implements ModInitializer {
-	public static final String ID = "shieldstun";
-	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
+public class ShieldStun implements ModInitializer {
+	public static final String MOD_ID = "shieldstun";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
 	public void onInitialize() {
 		ConfigManager.loadConfig();
-
-		CommandRegistrationCallback.EVENT.register((dispatcher, access, environment) -> Commands.register(dispatcher));
+		CommandRegistrationCallback.EVENT.register(Commands::register);
 	}
 }
--- src/main/java/me/libreh/shieldstun/mixin/LivingEntityMixin.java
+++ src/main/java/me/libreh/shieldstun/mixin/LivingEntityMixin.java
@@ -1,41 +1,37 @@
 package me.libreh.shieldstun.mixin;
 
-import me.libreh.shieldstun.config.ConfigCache;
-import me.libreh.shieldstun.config.ConfigManager;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Cancellable;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
-	@Shadow public abstract float getDamageBlockedAmount(ServerWorld world, DamageSource source, float amount);
-
 	@Shadow public abstract boolean isBlocking();
 
 	public LivingEntityMixin(EntityType<?> type, World world) {
 		super(type, world);
 	}
 
-	@Unique
-	private static final ConfigCache CONFIG = ConfigManager.getConfigCache();
-
-	@Inject(method = "damage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getDamageBlockedAmount(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/damage/DamageSource;F)F", shift = At.Shift.BEFORE), cancellable = true)
-	private void shieldStun$stunHit(ServerWorld world, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-		if (CONFIG.isStunEnabled() && this.isBlocking() &&
+	@WrapOperation(method = "damage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getDamageBlockedAmount(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/damage/DamageSource;F)F"))
+	private float notWorking(LivingEntity instance, ServerWorld world, DamageSource source, float amount, Operation<Float> original, @Cancellable CallbackInfoReturnable<Boolean> cir) {
+		if (this.isBlocking() &&
 				source.getSource() != null && source.getSource() instanceof ServerPlayerEntity
 		) {
-			getDamageBlockedAmount(world, source, amount);
 			cir.setReturnValue(false);
 		}
+		return original.call(instance, world, source, amount);
 	}
 }
