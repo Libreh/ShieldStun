d6aaa3da69e95bfcd75fd447c0a0f774c89272eb
Libreh <beverlydlashley@protonmail.com>
2025-06-28 00:01:54 +0200

    Move away from generic based config system

--- src/main/java/me/libreh/shieldstun/config/Config.java
+++ src/main/java/me/libreh/shieldstun/config/Config.java
@@ -1,38 +1,11 @@
 package me.libreh.shieldstun.config;
 
+import com.google.gson.annotations.SerializedName;
 import me.libreh.shieldstun.util.Constants;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 public class Config {
-    private final Map<String, ConfigOption<?>> options = new LinkedHashMap<>();
+    public static final Config DEFAULT = new Config();
 
-    public Config() {
-        registerOption(new ConfigOption<>(Constants.ENABLE_STUNS, true, Boolean.class));
-    }
-
-    private void registerOption(ConfigOption<?> option) {
-        options.put(option.getKey(), option);
-    }
-
-    public ConfigOption<?> getOption(String key) {
-        return options.get(key);
-    }
-
-    public Map<String, ConfigOption<?>> getOptions() {
-        return options;
-    }
-
-    public boolean getBoolean(String key) {
-        return getTypedValue(key, Boolean.class);
-    }
-
-    private <T> T getTypedValue(String key, Class<T> type) {
-        ConfigOption<?> option = options.get(key);
-        if (option != null && type.isAssignableFrom(option.getType())) {
-            return type.cast(option.get());
-        }
-        throw new IllegalArgumentException("Invalid config key or type: " + key);
-    }
+    @SerializedName(Constants.ENABLE_STUNS)
+    public boolean enableStuns;
 }
--- src/main/java/me/libreh/shieldstun/config/ConfigManager.java
+++ src/main/java/me/libreh/shieldstun/config/ConfigManager.java
@@ -2,77 +2,57 @@ package me.libreh.shieldstun.config;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import me.libreh.shieldstun.ModInit;
+import me.libreh.shieldstun.ShieldStun;
 import net.fabricmc.loader.api.FabricLoader;
 
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
 
 public class ConfigManager {
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-    private static final Path CONFIG_PATH = FabricLoader.getInstance().getConfigDir().resolve("shieldstun.json");
-
-    private static Config config;
+    private static final Path CONFIG_PATH = FabricLoader.getInstance().getConfigDir().resolve("%s.json".formatted(ShieldStun.MOD_ID));
+    private static Config CONFIG;
 
     public static Config getConfig() {
-        if (config == null) loadConfig();
-        return config;
-    }
-
-    private static ConfigCache configCache;
-
-    public static ConfigCache getConfigCache() {
-        if (configCache == null) configCache = new ConfigCache();
-        return configCache;
+        if (CONFIG == null) {
+            return Config.DEFAULT;
+        }
+        return CONFIG;
     }
 
     public static boolean loadConfig() {
+        Config oldConfig = CONFIG;
+        boolean success;
+
+        CONFIG = null;
         try {
-            config = new Config();
-            if (!Files.exists(CONFIG_PATH)) {
-                saveConfig();
-                return true;
-            }
+            File configFile = CONFIG_PATH.toFile();
 
-            JsonObject json = GSON.fromJson(new InputStreamReader(Files.newInputStream(CONFIG_PATH)), JsonObject.class);
-            for (Map.Entry<String, JsonElement> entry : json.entrySet()) {
-                ConfigOption<?> option = config.getOption(entry.getKey());
-                if (option != null) {
-                    setOptionValue(option, entry.getValue());
-                }
-            }
-            return true;
-        } catch (Exception e) {
-            ModInit.LOGGER.error("Failed to load config", e);
-            return false;
-        }
-    }
+            CONFIG = configFile.exists() ? GSON.fromJson(new InputStreamReader(new FileInputStream(configFile), "UTF-8"), Config.class) : new Config();
+            saveConfig();
 
-    private static void setOptionValue(ConfigOption<?> option, JsonElement element) {
-        if (option.getType() == Boolean.class && element.isJsonPrimitive()) {
-            ((ConfigOption<Boolean>) option).set(element.getAsBoolean());
+            success = true;
+        } catch (Exception exception) {
+            success = false;
+            CONFIG = oldConfig;
+            ShieldStun.LOGGER.error("Something went wrong while reading config!", exception);
         }
+
+        return success;
     }
 
     public static boolean saveConfig() {
+        boolean success;
+
         try {
-            JsonObject json = new JsonObject();
-            for (ConfigOption<?> option : config.getOptions().values()) {
-                if (option.getType() == Boolean.class) {
-                    json.addProperty(option.getKey(), (Boolean) option.get());
-                }
-            }
-            Files.createDirectories(CONFIG_PATH.getParent());
-            Files.writeString(CONFIG_PATH, GSON.toJson(json));
-            configCache.reload();
-            return true;
-        } catch (Exception e) {
-            ModInit.LOGGER.error("Failed to save config", e);
-            return false;
+            Files.writeString(CONFIG_PATH, GSON.toJson(CONFIG));
+            success = true;
+        } catch (Exception exception) {
+            success = false;
+            ShieldStun.LOGGER.error("Something went wrong while saving config!", exception);
         }
+
+        return success;
     }
 }
