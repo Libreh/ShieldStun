2f98b1a44baa0b7f984a1a5c2b348abe6993c8e0
Libreh <beverlydlashley@protonmail.com>
2025-07-07 16:14:56 +0200

    Code refactoring

--- src/main/java/me/libreh/shieldstun/config/Config.java
+++ src/main/java/me/libreh/shieldstun/config/Config.java
@@ -1,9 +1,8 @@
 package me.libreh.shieldstun.config;
 
 import com.google.gson.annotations.SerializedName;
-import me.libreh.shieldstun.util.Constants;
 
 public class Config {
-    @SerializedName(Constants.ENABLE_STUNS)
+    @SerializedName("enable_stuns")
     public boolean enableStuns = true;
 }
--- src/main/java/me/libreh/shieldstun/util/Util.java
+++
@@ -1,9 +0,0 @@
-package me.libreh.shieldstun.util;
-
-import me.libreh.shieldstun.ShieldStun;
-
-public class Util {
-    public static String formatPermission(String key) {
-        return "%s.%s".formatted(ShieldStun.MOD_ID, key);
-    }
-}
