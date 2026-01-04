8d53cd119e7cd1c978b219bca36b5b9f3d8e6a07
libreh <beverlydlashley@protonmail.com>
2026-01-04 16:59:59 +0100

    Fix previous impl, method isn't cancelled anymore.

--- src/main/java/me/libreh/shieldstun/mixin/LivingEntityMixin.java
+++ src/main/java/me/libreh/shieldstun/mixin/LivingEntityMixin.java
@@ -1,35 +1,52 @@
 package me.libreh.shieldstun.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Cancellable;
 import me.libreh.shieldstun.config.ConfigManager;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
-    @Shadow
-    public abstract float getYHeadRot();
-
-    public LivingEntityMixin(EntityType<?> type, Level level) {
-        super(type, level);
+    public LivingEntityMixin(EntityType<?> entityType, Level level) {
+        super(entityType, level);
     }
 
+    @Unique
+    private boolean blockedHit = false;
+
     @WrapOperation(method = "hurtServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;applyItemBlocking(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/damagesource/DamageSource;F)F"))
-    private float hurtServer(LivingEntity instance, ServerLevel world, DamageSource source, float amount, Operation<Float> original, @Cancellable CallbackInfoReturnable<Boolean> cir) {
-        float blockedDamage = original.call(instance, world, source, amount);
-        if (blockedDamage != 0.0F && ConfigManager.getConfig().enableStuns) {
-            cir.setReturnValue(false);
+    private float hurtServer(LivingEntity instance, ServerLevel serverLevel, DamageSource damageSource, float damageAmount, Operation<Float> original) {
+        float blockedAmount = original.call(instance, serverLevel, damageSource, damageAmount);
+        if (ConfigManager.getConfig().enableStuns && damageAmount != 0.0F) {
+            blockedHit = true;
+        } else {
+            blockedHit = false;
         }
-        return blockedDamage;
+        return blockedAmount;
+    }
+
+    @ModifyReturnValue(method = "hurtServer", at = @At("RETURN"))
+    private boolean hurtServerReturn(boolean original) {
+        if (blockedHit) {
+            this.invulnerableTime = 0;
+            return false;
+        }
+        return original;
     }
 }
