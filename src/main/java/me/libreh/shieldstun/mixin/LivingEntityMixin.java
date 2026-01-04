914e8457988bf6d1be9dca027f2ce4ae8b00bfb5
libreh <beverlydlashley@protonmail.com>
2026-01-04 17:01:07 +0100

    Simplify if statement

--- src/main/java/me/libreh/shieldstun/mixin/LivingEntityMixin.java
+++ src/main/java/me/libreh/shieldstun/mixin/LivingEntityMixin.java
@@ -26,11 +26,7 @@ public abstract class LivingEntityMixin extends Entity {
     @WrapOperation(method = "hurtServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;applyItemBlocking(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/damagesource/DamageSource;F)F"))
     private float hurtServer(LivingEntity instance, ServerLevel serverLevel, DamageSource damageSource, float damageAmount, Operation<Float> original) {
         float blockedAmount = original.call(instance, serverLevel, damageSource, damageAmount);
-        if (ConfigManager.getConfig().enableStuns && damageAmount != 0.0F) {
-            blockedHit = true;
-        } else {
-            blockedHit = false;
-        }
+        blockedHit = ConfigManager.getConfig().enableStuns && damageAmount != 0.0F;
         return blockedAmount;
     }
 
