8831cd8774464306bd1a0c4465d303aa4a3cc864
libreh <beverlydlashley@protonmail.com>
2026-01-04 18:16:49 +0100

    Add GenericModInfo utility class

--- src/main/java/me/libreh/shieldstun/ShieldStun.java
+++ src/main/java/me/libreh/shieldstun/ShieldStun.java
@@ -1,20 +1,24 @@
 package me.libreh.shieldstun;
 
-import me.libreh.shieldstun.command.Commands;
+import me.libreh.shieldstun.command.ShieldStunCommand;
 import me.libreh.shieldstun.config.ConfigManager;
+import me.libreh.shieldstun.util.GenericModInfo;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ShieldStun implements ModInitializer {
 	public static final String MOD_ID = "shieldstun";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static ModContainer CONTAINER = FabricLoader.getInstance().getModContainer(MOD_ID).get();
 
     @Override
 	public void onInitialize() {
-		ConfigManager.loadConfig();
-		CommandRegistrationCallback.EVENT.register((dispatcher, access, environment) -> Commands.register(dispatcher));
+        GenericModInfo.build(CONTAINER, MOD_ID, LOGGER, true, true, 0xFF80EA);
+
 		ConfigManager.load();
 		CommandRegistrationCallback.EVENT.register((dispatcher, access, environment) -> ShieldStunCommand.register(dispatcher));
 	}
