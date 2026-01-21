eb3f9de35ea38e735eb823a8aa47f610e22b9a99
libreh <beverlydlashley@protonmail.com>
2026-01-21 22:14:46 +0100

    Optimize imports

--- src/main/java/me/libreh/shieldstun/config/ConfigManager.java
+++ src/main/java/me/libreh/shieldstun/config/ConfigManager.java
@@ -7,7 +7,6 @@ import me.libreh.shieldstun.ShieldStun;
 import net.fabricmc.loader.api.FabricLoader;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
